873a97638e7e92ccdab970bc424e23e3dcb47bbe
anditp <anditp@gmail.com>
2024-05-06 10:20:33 +0200

    Changes

--- training.py
+++ training.py
@@ -4,6 +4,7 @@ import os
 from data.lagrangian_datatools import dataset_from_file
 from learner import ScIDiffLearner
 from torch.nn.parallel import DistributedDataParallel
+import logger
 
 def _train_impl(replica_id, model, dataset, params):
     torch.backends.cudnn.benchmark = True
@@ -26,6 +27,7 @@ def train_distributed(replica_id, replica_count, port, model_params):
     torch.distributed.init_process_group('nccl', rank=replica_id, world_size=replica_count)
     dataset = dataset_from_file(model_params["data_path"], model_params["batch_size"], model_params["levels"], is_distributed=True, num_workers=2, coordinate=model_params.coordinate)
     device = torch.device('cuda', replica_id)
+    logger.log(device)
     torch.cuda.set_device(device)
     model = ScIDiff(model_params).to(device)
     model = DistributedDataParallel(model, device_ids=[replica_id])
