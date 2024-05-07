a45397833644d719bf10151ec0a21b9ee7c711c3
anditp <anditp@gmail.com>
2024-05-07 08:07:39 +0200

    Changes

--- Statistics.py
+++ Statistics.py
@@ -6,8 +6,14 @@ import os
 v = np.load("/Users/andrei/Desktop/bl/velocities.npy", mmap_mode = "r").transpose((0, 2, 1))[:30000,0,:]
 v = np.expand_dims(v, 1)
 #%%
-w = np.load("/Users/andrei/Desktop/bl/generated_simple_3d.npy", mmap_mode = "r").transpose((0, 2, 1))[:,0,:]
-w = np.expand_dims(w, 1)
+w0 = np.load("/Users/andrei/Desktop/bl/generated_simple_3d.npy", mmap_mode = "r").transpose((0, 2, 1))[:,0,:]
+w1 = np.load("/Users/andrei/Desktop/bl/generated_simple_3d.npy", mmap_mode = "r").transpose((0, 2, 1))[:,1,:]
+w2 = np.load("/Users/andrei/Desktop/bl/generated_simple_3d.npy", mmap_mode = "r").transpose((0, 2, 1))[:,2,:]
+w0 = np.expand_dims(w0, 1)
+w1 = np.expand_dims(w1, 1)
+w2 = np.expand_dims(w2, 1)
+
+w = np.concatenate((w0, w1, w2), axis = 1)
 
 #%%
 plt.plot(w[0].flatten())
@@ -59,7 +65,7 @@ num_bins = 1000
 
 fig, ax = plt.subplots()
 ax.hist(vals_T, num_bins, alpha = 0.5, density=True, label = "Sampled")
-#ax.hist(vals_S, num_bins, alpha = 0.5, density=True, label = "Simulated")
+ax.hist(vals_S, num_bins, alpha = 0.5, density=True, label = "Simulated")
 ax.set_xlabel('Value')
 ax.set_ylabel('Probability density')
 ax.set_title('Histogram')
