579a03a06748b19d79e83eaccedfc36ed5d26152
anditp <anditp@gmail.com>
2024-05-08 02:13:03 +0200

    Changes

--- infer.py
+++ infer.py
@@ -67,6 +67,7 @@ def generate_trajectories(args, model, model_params, device, fast_sampling=False
         x_0 = np.random.randn(B, model_params.num_coords, model_params.traj_len)
         if model_params.type in ["fourier", "simple_fourier"]:
             gen_x = fourier_nscales(x_0, scales=model_params.levels)
+            logger.log(gen_x)
             gen_x = _nested_map(gen_x, lambda x: x.to(device))
         else:
             gen_x = interpolate_nscales(x_0, scales=model_params.levels)
