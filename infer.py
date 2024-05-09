6b2c74d43e4eadf8243c5e5f21c2b167933002c2
anditp <anditp@gmail.com>
2024-05-09 11:10:12 +0200

    Changes

--- infer.py
+++ infer.py
@@ -133,7 +133,7 @@ def main(args):
     for _ in range(N//B):
         logger.log("Iteration %d \n" % _)
         gen_samples = generate_trajectories(args, model, model_params, device, fast_sampling=args.fast)
-        batches_gen.append(gen_samples[2])
+        batches_gen.append(gen_samples[3])
     # concatenate batches in a single one
     gen_samples = torch.cat(batches_gen, dim=0)
     # permute to (N, length, num_coords)
