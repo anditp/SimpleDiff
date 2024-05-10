ab955dc8559eff7017ce989762a6dd7a8fb6b1d7
anditp <anditp@gmail.com>
2024-05-10 16:37:03 +0200

    Changes

--- infer.py
+++ infer.py
@@ -78,6 +78,7 @@ def generate_trajectories_mr(args, model, model_params, device):
             # T-1 steps of denoising
             # we are iterating backwards
             for t in range(len(alpha) - 1, -1, -1):
+                logger.log(t)
                 c1 = 1 / alpha[t]**0.5
                 c2 = beta[t] / (1 - alpha_cum[t])**0.5
                 pred_noise = model(gen_x[level], torch.tensor([t], device=device), condition)
