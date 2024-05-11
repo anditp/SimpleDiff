56ca7c4084c5083a0d9a0e20bbdeff48379e1c6f
anditp <anditp@gmail.com>
2024-05-11 13:27:58 +0200

    Changes

--- infer.py
+++ infer.py
@@ -129,14 +129,14 @@ def generate_trajectories(args, model, model_params, device, fast_sampling=False
         """
         # get random noise vector at several scales
         # random tensor must be of shape (N, num_coords, length + padding)
-        x_0 = np.random.randn(B, model_params.num_coords, model_params.traj_len * model_params.levels)
-        #if model_params.type in ["fourier", "simple_fourier"]:
-         #   gen_x = fourier_nscales(x_0, scales = model_params.levels)
-          #  gen_x = _nested_map(gen_x, lambda x: x.to(device))
+        x_0 = np.random.randn(B, model_params.num_coords, model_params.traj_len)
+        if model_params.type in ["fourier", "simple_fourier"]:
+            gen_x = fourier_nscales(x_0, scales = model_params.levels)
+            gen_x = _nested_map(gen_x, lambda x: x.to(device))
         #else:
          #   gen_x = interpolate_nscales(x_0, scales=model_params.levels)
-        trajectories = np.split(x_0, model_params.levels, axis = -1)
-        gen_x = {}
+        #trajectories = np.split(x_0, model_params.levels, axis = -1)
+        #gen_x = {}
         for level, noise in enumerate(trajectories):
             gen_x[level] = torch.Tensor(noise).to(device)
         # T-1 steps of denoising
