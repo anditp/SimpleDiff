8cf6bb20ca734b061661603bab00227565c31bb0
anditp <anditp@gmail.com>
2024-05-11 18:07:35 +0200

    Changes

--- data/lagrangian_datatools.py
+++ data/lagrangian_datatools.py
@@ -238,10 +238,7 @@ def dataset_from_file(npy_fname,
         transforms.append(TakeOneCoord(coord=coordinate))
     dataset = ParticleDataset(npy_fname, transform=Compose(transforms))
     
-    if fourier:
-        col = Collator_fourier(levels = levels)
-    else:
-        col = Collator(levels = levels)
+    col = Collator_fourier(levels = levels)
     
     
     return torch.utils.data.DataLoader(
